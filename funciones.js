d6dcde4f2ad0a37582fbeabe4c36c26b20cdbc42
Karin Munoz <ksmunoz@superservicios.gov.co>
2022-05-09 16:47:51 -0500

    funciones en javascript

---
+++ funciones.js
@@ -0,0 +1,18 @@
+// Declarativas
+
+function miFuncion() {
+   return 3;
+}
+
+miFuncion();
+
+// Expresión , expresivas
+
+var miFuncion = function(a,b) {
+    return a+b;
+}
+
+
+miFuncion(2,3);
+
+
