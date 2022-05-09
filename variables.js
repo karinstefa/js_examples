963e641c96993c2b3280bc2dea844e73b518a247
Karin Munoz <ksmunoz@superservicios.gov.co>
2022-05-09 16:47:25 -0500

    examples de functions and variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+
+//representación de un luagar en memmoria 
+
+var nombre = "Juan";
+var apellido = "Perez";
+
+var direccion = "Calle falsa 123";
+
+//declaración de una variable
+var edad;
+
+//inicialización de una variable
+edad = 30;
+
+// variables de array 
+
+var elementos = ["Hola", "Mundo", "!"];
+
