6338020458f6b4d065f5f9e17c15ec3700da1458
Karin Munoz <ksmunoz@superservicios.gov.co>
2022-05-16 17:28:19 -0500

    add examples functions

--- funciones.js
+++ funciones.js
@@ -15,4 +15,29 @@ var miFuncion = function(a,b) {
 
 miFuncion(2,3);
 
+//-----------------------------------------------------
+//Funciones Declarativas:
+//En las funciones declarativas, utilizamos la palabra reservada
+// function al inicio para poder declarar la función:
+
+function saludar(nombre) {
+	console.log(`Hola ${nombre}`);
+}
+
+saludar('Diego');
+
+//-----------------------------------------------------
+//Expresión de función:
+//En la expresión de función, la declaración se inicia con la
+// palabra reservada var, donde se generará una variable que guardará una función anónima.
+
+var nombre = function(nombre){
+    console.log(`Hola ${nombre}`)
+}
+
+nombre('Diego');
+
+
+
+
 
